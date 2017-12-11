a8920fc706c93f962d656270dc3029ba414b9434
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-12 04:15:33 +0530

    Revert "drivers: wakeup: there's no much point in running the blockers during screen on"
    This reverts commit fa783b7caa51223a3c80d26e74f2cbf040a1d2ef.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -16,7 +16,6 @@
 #include <linux/debugfs.h>
 #include <linux/types.h>
 #include <linux/moduleparam.h>
-#include <linux/display_state.h>
 #include <trace/events/power.h>
 
 #include "power.h"
@@ -439,7 +438,7 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 {
 	unsigned int wslen = 0;
 
-	if (!is_display_on() && ws && ws->active) {
+	if (ws && ws->active) {
 		wslen = strlen(ws->name);
 
 		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen)) ||
