9d5f5d4de454b66112fc90e0e6d5799502c10257
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-11 21:42:07 +0530

    Revert "net:Add sysctl_max_skb_frags"
    This reverts commit df93393787a3fc3b994c5061a890b0250bae0fbc.

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -966,7 +966,7 @@ new_segment:
 
 		i = skb_shinfo(skb)->nr_frags;
 		can_coalesce = skb_can_coalesce(skb, i, page, offset);
-		if (!can_coalesce && i >= sysctl_max_skb_frags) {
+		if (!can_coalesce && i >= MAX_SKB_FRAGS) {
 			tcp_mark_push(tp, skb);
 			goto new_segment;
 		}
@@ -1252,7 +1252,7 @@ new_segment:
 
 				if (!skb_can_coalesce(skb, i, pfrag->page,
 						      pfrag->offset)) {
-					if (i == sysctl_max_skb_frags || !sg) {
+					if (i == MAX_SKB_FRAGS || !sg) {
 						tcp_mark_push(tp, skb);
 						goto new_segment;
 					}
