ce00ae247665d356d9baf52b59ca524556f4a4d2
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-12 04:14:36 +0530

    Revert "drivers: wakeup: allow 1 wakeup from IPA_WS every now and then"
    This reverts commit 835da3c041663e4663440c232cb600bc8e556e73.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -442,8 +442,7 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 	if (ws) {
 		wslen = strlen(ws->name);
 
-		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen) &&
-				ws->active) ||
+		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen)) ||
 			(!enable_wlan_extscan_wl_ws &&
 				!strncmp(ws->name, "wlan_extscan_wl", wslen)) ||
 			(!enable_qcom_rx_wakelock_ws &&
