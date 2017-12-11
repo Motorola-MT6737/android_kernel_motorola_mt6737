a79b584cdf5ae531f633079fa85f67be2f07c284
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-12 04:05:00 +0530

    Revert "drivers: wakeup: allow to request and count wakelocks with screen on."
    This reverts commit eb549e1f93cc253b344afd6d0b0dd075aefcd8b5.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -442,7 +442,8 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 	if (ws) {
 		wslen = strlen(ws->name);
 
-		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen)) ||
+		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen) &&
+				ws->active) ||
 			(!enable_wlan_extscan_wl_ws &&
 				!strncmp(ws->name, "wlan_extscan_wl", wslen)) ||
 			(!enable_qcom_rx_wakelock_ws &&
@@ -530,14 +531,16 @@ static void wakeup_source_activate(struct wakeup_source *ws)
  */
 static void wakeup_source_report_event(struct wakeup_source *ws)
 {
-	if (!wakeup_source_blocker(ws)) {
-		ws->event_count++;
-		/* This is racy, but the counter is approximate anyway. */
-		if (events_check_enabled)
-			ws->wakeup_count++;
+	if (!is_display_on()) {
+		if (!wakeup_source_blocker(ws)) {
+			ws->event_count++;
+			/* This is racy, but the counter is approximate anyway. */
+			if (events_check_enabled)
+				ws->wakeup_count++;
 
-		if (!ws->active)
-			wakeup_source_activate(ws);
+			if (!ws->active)
+				wakeup_source_activate(ws);
+		}
 	}
 }
 
